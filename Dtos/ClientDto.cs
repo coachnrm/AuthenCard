b2621ae901858934fb5eab735423a5e457a50b88
coachnrm <nuttapong_coach@hotmail.com>
2024-10-21 15:13:32 +0700

    add code

--- Dtos/ClientDto.cs
+++ Dtos/ClientDto.cs
@@ -2,39 +2,38 @@ namespace AuthenCard.Dtos
 {
     public class ClientDto
     {
-        public string hosGuid { get; set; }
-        public string vn { get; set; }
-        public string hn { get; set; }
+        public string vn { get; set; } 
+        public string hn { get; set; } 
         public object an { get; set; }
         public string vstdate { get; set; } 
         public string vsttime { get; set; } 
-        public string doctor { get; set; }
-        public string hospmain { get; set; }
-        public string hospsub { get; set; }
+        public string doctor { get; set; } 
+        public string hospmain { get; set; } 
+        public string hospsub { get; set; } 
         public int? oqueue { get; set; } 
-        public string ovstist { get; set; }
-        public string ovstost { get; set; }
-        public string pttype { get; set; }
-        public string pttypeno { get; set; }
+        public string ovstist { get; set; } 
+        public string ovstost { get; set; } 
+        public string pttype { get; set; } 
+        public string pttypeno { get; set; } 
         public object rfrics { get; set; }
         public object rfrilct { get; set; } 
         public string rfrocs { get; set; } 
         public string rfrolct { get; set; } 
         public string spclty { get; set; }
         public object rcptDisease { get; set; } 
-        public string hcode { get; set; }
-        public string curDep { get; set; }
-        public string curDepBusy { get; set; }
-        public string lastDep { get; set; }
+        public string hcode { get; set; } 
+        public string curDep { get; set; } 
+        public string curDepBusy { get; set; } 
+        public string lastDep { get; set; } 
         public string curDepTime { get; set; } 
         public object rxQueue { get; set; } 
         public object diagText { get; set; } 
         public int ptSubtype { get; set; }
-        public string mainDep { get; set; }
+        public string mainDep { get; set; } 
         public int mainDepQueue { get; set; }
         public object financeSummaryDate { get; set; } 
-        public string visitType { get; set; }
-        public string nodeId { get; set; } 
+        public string visitType { get; set; } 
+        public string nodeId { get; set; }  
         public object contractId { get; set; } 
         public string waiting { get; set; } 
         public object rfriIcd10 { get; set; } 
@@ -55,6 +54,6 @@ namespace AuthenCard.Dtos
         public object anonymousVn { get; set; } 
         public object ptCapabilityTypeId { get; set; } 
         public object atHospital { get; set; } 
-        public string ovstKey { get; set; }
+        public string ovstKey { get; set; } 
     }
 }
